3fa4f0d36e971905e0aa9982f29f79fed5c34199
rajeshkudi <rajeshkudi@gmail.com>
2025-05-16 17:22:29 +0530

    Setting Up

---
+++ lbdemo/src/main/java/com/example/demo/LbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbdemoApplication.class, args);
+	}
+
+}
---
+++ lbdemo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	private int i=1;
+	@GetMapping("/get")
+	public String get() throws UnknownHostException {
+		InetAddress localhost = InetAddress.getLocalHost();
+		System.out.println("hitting " + i++);
+		return "Hi Rajesh ! Welcome to Our App : From " + localhost.getHostAddress().trim() ;
+	}
+	
+   
+	
+}
